1292397a7c60841d0660c4fa21595cf634efc474
Timothy Ludwig <tludwig01@gmail.com>
2017-06-20 05:05:55 -0400

    created game logic

--- memory-game/main.js
+++ memory-game/main.js
@@ -1,11 +1,8 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-cardsInPlay.push("queen", "king");
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	}
@@ -13,9 +10,16 @@ if (cardsInPlay.length === 2) {
 		alert("Sorry, try again.");
 	}
 }
+}
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
 
 
-
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
