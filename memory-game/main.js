9ad8ce2d7b1b3fbb18fafa761c04a9c5e6221546
Timothy Ludwig <tludwig01@gmail.com>
2017-06-20 11:59:56 -0400

    created game logic

--- memory-game/main.js
+++ memory-game/main.js
@@ -1,25 +1,48 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		image: "images/queen-of-hearts.png",
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		image: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		image: "images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		image: "images/king-of-diamonds",
+	}
+];
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
 	if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
-	}
-	else {
+		}
+		else {
 		alert("Sorry, try again.");
+		}
 	}
-}
 }
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].image);
+	console.log(cards[cardId].suit);
 
 	checkForMatch();
 };
 
 flipCard(0);
 flipCard(2);
-
-
