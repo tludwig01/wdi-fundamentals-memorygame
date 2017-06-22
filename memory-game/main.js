440cd3c525adb9b3a40b15bba816b739566b6d30
Timothy Ludwig <tludwig01@gmail.com>
2017-06-22 10:23:15 -0400

    created game logic

--- memory-game/main.js
+++ memory-game/main.js
@@ -17,7 +17,7 @@ var cards = [
 	{
 		rank: "king",
 		suit: "diamonds",
-		image: "images/king-of-diamonds",
+		image: "images/king-of-diamonds.png",
 	}
 ];
 
@@ -33,16 +33,29 @@ var checkForMatch = function () {
 		alert("Sorry, try again.");
 		}
 	}
-}
+};
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].image);
 	console.log(cards[cardId].suit);
-
+	this.setAttribute('src', cards[cardId].image);
+	if (cardsInPlay.length === 2){
 	checkForMatch();
+	}
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function (){
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		document.getElementById('game-board').appendChild(cardElement);
+		cardElement.addEventListener('click', flipCard);
+	}
+};
+
+createBoard();
+
